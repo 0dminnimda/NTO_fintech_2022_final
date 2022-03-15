bc40ab911ae1650521303b82c0faceb61805d9fb
Gusarich <danielsedovzzz@gmail.com>
2022-03-15 14:14:19 +0300

    rewrite eip712 (4th attempt)

--- blockchain/contracts/RentalAgreement.sol
+++ blockchain/contracts/RentalAgreement.sol
@@ -7,23 +7,6 @@ struct Sign {
     bytes32 s;
 }
 
-struct EIP712Domain {
-    string name;
-    string version;
-    address verifyingContract;
-}
-
-struct RentalPermit {
-    uint256 deadline;
-    address tenant;
-    uint256 rentalRate;
-    uint256 billingPeriodDuration;
-    uint256 billingsCount;
-}
-
-bytes32 constant EIP712Domain_TYPEHASH = keccak256("EIP712Domain(string name,string version,address verifyingContract)");
-bytes32 constant RentalPermit_TYPEHASH = keccak256("RentalPermit(uint256 deadline,address tenant,uint256 rentalRate,uint256 billingPeriodDuration,uint256 billingsCount)");
-
 contract RentalAgreement {
     uint roomInternalId_;
     uint deadline_;
@@ -34,55 +17,30 @@ contract RentalAgreement {
     uint rentEndTime_;
     address landLord_;
     address tenant_;
-    bytes32 DOMAIN_SEPARATOR;
+
+    bytes32 public DOMAIN_SEPARATOR;
+    bytes32 public constant PERMIT_TYPEHASH = keccak256("RentalPermit(uint256 deadline,address tenant,uint256 rentalRate,uint256 billingPeriodDuration,uint256 billingsCount)");
 
     constructor (uint roomInternalId) {
         landLord_ = msg.sender;
         roomInternalId_ = roomInternalId;
-        DOMAIN_SEPARATOR = hash(EIP712Domain({
-            name: "Rental Agreement",
-            version: '1.0',
-            verifyingContract: address(this)
-        }));
-    }
-
-    function hash(EIP712Domain memory domain) internal pure returns (bytes32) {
-        return keccak256(abi.encode(
-            EIP712Domain_TYPEHASH,
-            keccak256(bytes(domain.name)),
-            keccak256(bytes(domain.version)),
-            domain.verifyingContract
+        DOMAIN_SEPARATOR = keccak256(abi.encode(
+            keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"),
+            keccak256(bytes("Rental Agreement")),
+            keccak256(bytes("1.0")),
+            address(this)
         ));
-    }
-
-    function hash(RentalPermit memory rp) internal pure returns (bytes32) {
-        return keccak256(abi.encode(
-            RentalPermit_TYPEHASH,
-            rp.deadline,
-            rp.tenant,
-            rp.rentalRate,
-            rp.billingPeriodDuration,
-            rp.billingsCount
-        ));
-    }
-
-    function verify(RentalPermit memory rp, uint8 v, bytes32 r, bytes32 s) internal view returns (bool) {
-        bytes32 digest = keccak256(abi.encodePacked(
-            "\x19\x01",
-            DOMAIN_SEPARATOR,
-            hash(rp)
-        ));
-        return ecrecover(digest, v, r, s) == landLord_;
     }
 
     function rent (uint deadline, address tenant, uint rentalRate, uint billingPeriodDuration, uint billingsCount, Sign calldata landlordSign) payable public {
         if (tenant_ != address(0)) revert("The contract is being in not allowed state");
-        if (!verify(
-            RentalPermit(deadline, tenant, rentalRate, billingPeriodDuration, billingsCount),
-            landlordSign.v,
-            landlordSign.r,
-            landlordSign.s
-        )) revert("Invalid landlord sign");
+
+        bytes32 digest = keccak256(abi.encodePacked(
+            "\x19\x01",
+            DOMAIN_SEPARATOR,
+            keccak256(abi.encode(PERMIT_TYPEHASH, deadline, tenant, rentalRate, billingPeriodDuration, billingsCount))
+        ));
+        if (landLord_ != ecrecover(digest, landlordSign.v, landlordSign.r, landlordSign.s)) revert("Invalid landlord sign");
 
         if (deadline > block.timestamp) revert("The operation is outdated");
         if (tenant != msg.sender) revert("The caller account and the account specified as a tenant do not match");
