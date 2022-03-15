ca0687f000faa1d694a1c62966a1426d0d3d1b49
0dminnimda <0dminnimda@gmail.com>
2022-03-15 12:41:48 +0300

    Implement start of the authentication

--- backend/sugomA/AmogusApp/models.py
+++ backend/sugomA/AmogusApp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Authentication(models.Model):
+    address = models.TextField("address", default="")
+    # address = models.CharField("address", max_length=128)
+    isLandlord = models.BooleanField("isLandlord")
+
+    def __str__(self):
+        return str(type(self)) + "(" + self.address + ", " + self.isLandlord + ")"
--- backend/sugomA/settings.py
+++ backend/sugomA/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "sugomA.AmogusApp",
+    "graphene_django",
 ]
 
 MIDDLEWARE = [
@@ -121,3 +123,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+GRAPHENE = {
+    "SCHEMA": "sugomA.schema.schema"
+}
