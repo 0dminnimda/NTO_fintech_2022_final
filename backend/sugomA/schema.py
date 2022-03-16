a3f01157a22d4e691460fa98343686e125535a05
0dminnimda <0dminnimda@gmail.com>
2022-03-16 21:32:16 +0300

    sugomA\schema.py: resolve authentication and authenticate

--- backend/sugomA/schema.py
+++ backend/sugomA/schema.py
@@ -1,3 +1,4 @@
+import os
 import secrets
 
 from ariadne import (MutationType, ObjectType, QueryType, gql,
@@ -85,7 +86,10 @@ query = QueryType()
 
 @query.field("authentication")
 def resolve_authentication(_, info):
-    return None
+    if not code_smell.successfull_auth:
+        return None
+
+    return Authentication.objects.get(address=code_smell.address)
 
 
 mutation = MutationType()
@@ -114,16 +118,21 @@ def resolve_request_authentication(_, info, address):
 
 @mutation.field("authenticate")
 def resolve_authenticate(_, info, address, signedMessage):
-    raise Exception("A")
     # if not code_smell.get("requested_auth", False):
 
+    if code_smell.requested_auth == 0:
+        raise Exception("A")
+
+    code_smell.successfull_auth = True
+    code_smell.address = address
     authentications = Authentication.objects.filter(address=address)
 
     if len(authentications) == 0:
         return Authentication.objects.create(
             address=address, isLandlord=False)
 
-    return authentications[0]
+    return Authentication.objects.create(
+        address=address, isLandlord=os.environ.get("LANDLORD_ADDRESS", False))
 
 
 # authentication = ObjectType("Authentication")
