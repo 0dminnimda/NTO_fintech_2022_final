0c3f89d6265fbc9db5369777de99d29fb102d261
0dminnimda <0dminnimda@gmail.com>
2022-03-16 19:33:09 +0300

    Update sugomA\urls.py: update hack and format errors

--- backend/sugomA/urls.py
+++ backend/sugomA/urls.py
@@ -14,18 +14,59 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+import json
+from ariadne import format_error
+
+
 # from graphene_django.views import GraphQLView
 from ariadne_django.views import GraphQLView
 from django.contrib import admin
+from django.http.response import JsonResponse
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 
 from .AmogusApp.views import home
-from .schema import schema
+from .schema import schema, code_smell
+
+
+def track(view):
+    def wrapper(*args, **kwargs):
+        response = view(*args, **kwargs)
+
+        if isinstance(response, JsonResponse):
+            data = json.loads(response.content)
+            if "data" in data and "errors" in data:
+                del data["data"]
+            response = JsonResponse(data)  # json.dumps(data)
+        else:
+            code_smell.clear()
+
+        return response
+
+    wrapper.csrf_exempt = True
+    return wrapper
+
+
+mapping = {"A": {"message": "Authentication failed"}}
+
+
+def my_format_error(error, debug: bool = False) -> dict:
+    # # Create formatted error data
+    # formatted = error.formatted
+    # # Replace original error message with custom one
+    # formatted["message"] = "INTERNAL SERVER ERROR"
+
+    result = mapping.get(error.message, None)
+    if result is not None:
+        return result
+
+    return format_error(error, debug)
+
 
 urlpatterns = [
     path("", home),
     path("admin/", admin.site.urls),
-    path('graphql/', GraphQLView.as_view(schema=schema), name='graphql'),
+    path('graphql/', track(GraphQLView.as_view(
+        schema=schema, error_formatter=my_format_error)), name='graphql'),
     # path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True))),
 ]
