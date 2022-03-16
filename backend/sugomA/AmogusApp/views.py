79acea3fd382dcfeeb55c0f6ed81e25b0a56ef83
0dminnimda <0dminnimda@gmail.com>
2022-03-16 10:23:49 +0300

    Update backend\sugomA\AmogusApp\views.py
    [skip ci]

--- backend/sugomA/AmogusApp/views.py
+++ backend/sugomA/AmogusApp/views.py
@@ -4,5 +4,5 @@ from django.http import HttpResponse
 
 # def MetaMask_auth(request):
 def home(request):
-    return render(request, 'home.html')
+    return render(request, "home.html")
     # return HttpResponse("You're voting on question")
