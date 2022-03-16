9d02b8755942c764c30638a5f90ad16b9bff262e
0dminnimda <0dminnimda@gmail.com>
2022-03-16 19:57:02 +0300

    Update sugomA\settings.py: allow all hosts

--- backend/sugomA/settings.py
+++ backend/sugomA/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-kg(lg^fzkpniqar3fcktii43(yc8gz%*nwvp@va%y$w=bjxga6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["solution-web", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
