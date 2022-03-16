c8557b29a121d4ab6a0719fe8c54f99b97f9867a
0dminnimda <0dminnimda@gmail.com>
2022-03-16 19:24:38 +0300

    sugomA\settings.py: debug=false

--- backend/sugomA/settings.py
+++ backend/sugomA/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-kg(lg^fzkpniqar3fcktii43(yc8gz%*nwvp@va%y$w=bjxga6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["solution-web", "localhost", "127.0.0.1"]
 
