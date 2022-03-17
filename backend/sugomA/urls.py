0f819e0a913a585835fd2774c9f02b90e98d4fb0
0dminnimda <0dminnimda@gmail.com>
2022-03-17 15:21:41 +0300

    sugomA\urls.py: fuck that shit

--- backend/sugomA/urls.py
+++ backend/sugomA/urls.py
@@ -15,51 +15,28 @@ Including another URLconf
 """
 
 import json
+from typing import cast
+
 from ariadne import format_error
-
-
-# from graphene_django.views import GraphQLView
-from ariadne_django.views import GraphQLView
+from ariadne.constants import DATA_TYPE_JSON, DATA_TYPE_MULTIPART
+from ariadne.exceptions import HttpBadRequestError
+from ariadne.file_uploads import combine_multipart_data
+from ariadne.graphql import graphql_sync
+from django.conf import settings
 from django.contrib import admin
-from django.http.response import JsonResponse
+from django.http import HttpRequest, HttpResponseBadRequest, JsonResponse
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
+from graphql import GraphQLSchema
 
-from .AmogusApp.views import home, check
-from .schema import schema, code_smell
-
-
-def track(view):
-    def wrapper(*args, **kwargs):
-        request, *_ = args
-        # if request.method == "POST":
-        code_smell.storage = request.session
-
-        response = view(*args, **kwargs)
-
-        if code_smell["requested_auth"] > 0:
-            code_smell["requested_auth"] -= 1
-
-        if isinstance(response, JsonResponse):
-            data = json.loads(response.content)
-            if "data" in data and "errors" in data:
-                del data["data"]
-            response = JsonResponse(data)  # json.dumps(data)
-
-        return response
-
-    wrapper.csrf_exempt = True
-    return wrapper
-
+from .AmogusApp.views import check, home
+from .schema import code_smell, schema
 
 mapping = {"A": {"message": "Authentication failed"}}
 
 
 def my_format_error(error, debug: bool = False) -> dict:
-    # # Create formatted error data
     # formatted = error.formatted
-    # # Replace original error message with custom one
-    # formatted["message"] = "INTERNAL SERVER ERROR"
 
     result = mapping.get(error.message, None)
     if result is not None:
@@ -68,11 +45,99 @@ def my_format_error(error, debug: bool = False) -> dict:
     return format_error(error, debug)
 
 
+def extract_data_from_request(request: HttpRequest):
+    content_type = request.content_type or ""
+    content_type = content_type.split(";")[0]
+
+    if content_type == DATA_TYPE_JSON:
+        try:
+            return json.loads(request.body)
+        except (TypeError, ValueError) as ex:
+            raise HttpBadRequestError(
+                "Request body is not a valid JSON") from ex
+    if content_type == DATA_TYPE_MULTIPART:
+        try:
+            operations = json.loads(request.POST.get("operations", "{}"))
+        except (TypeError, ValueError) as ex:
+            raise HttpBadRequestError(
+                "Request 'operations' multipart field is not a valid JSON") from ex
+        try:
+            files_map = json.loads(request.POST.get("map", "{}"))
+        except (TypeError, ValueError) as ex:
+            raise HttpBadRequestError(
+                "Request 'map' multipart field is not a valid JSON") from ex
+
+        return combine_multipart_data(operations, files_map, request.FILES)
+
+    raise HttpBadRequestError(
+        "Posted content must be of type {} or {}".format(
+            DATA_TYPE_JSON, DATA_TYPE_MULTIPART))
+
+
+def get_context_for_request(context_value, request: HttpRequest):
+    if callable(context_value):
+        return context_value(request)
+    return context_value or {"request": request}
+
+
+def get_extensions_for_request(extensions, request: HttpRequest, context):
+    if callable(extensions):
+        return extensions(request, context)
+    return extensions
+
+
+def get_kwargs_graphql(request: HttpRequest) -> dict:
+    context_value = get_context_for_request(None, request)
+    extensions = get_extensions_for_request(None, request, context_value)
+
+    # http_method_names = ["get", "post", "options"]
+    # template_name = "ariadne_django/graphql_playground.html"
+    # playground_options: Optional[dict] = None
+    # schema: Optional[GraphQLSchema] = None
+
+    return {
+        "context_value": context_value,
+        "root_value": None,
+        "validation_rules": None,
+        "debug": settings.DEBUG,
+        "introspection": True,
+        "logger": None,
+        "error_formatter": my_format_error,
+        "extensions": extensions,
+        "middleware": None,
+    }
+
+
+@csrf_exempt
+def graphql_view(request: HttpRequest):
+    try:
+        data = extract_data_from_request(request)
+    except HttpBadRequestError as error:
+        return HttpResponseBadRequest(error.message)
+
+    code_smell.storage = request.COOKIES
+
+    success, result = graphql_sync(
+        cast(GraphQLSchema, schema), data, **get_kwargs_graphql(request))
+    status_code = 200 if success else 400
+
+    if code_smell["requested_auth"] > 0:
+        code_smell["requested_auth"] -= 1
+
+    if "data" in result and "errors" in result:
+        del result["data"]
+
+    response = JsonResponse(result, status=status_code)
+
+    for n, v in code_smell.storage.items():
+        response.set_cookie(n, v)
+
+    return response
+
+
 urlpatterns = [
-    path("", home),
+    path("", csrf_exempt(home)),
     path("admin/", admin.site.urls),
-    path('graphql', track(GraphQLView.as_view(
-        schema=schema, error_formatter=my_format_error)), name='graphql'),
-    path("check", check),
-    # path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True))),
+    path('graphql', graphql_view, name='graphql'),
+    path("check", csrf_exempt(check)),
 ]
