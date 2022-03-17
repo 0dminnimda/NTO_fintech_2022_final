9c7abb5ae3dda82afeda3bac99c0249bb5acf431
Gusarich <danielsedovzzz@gmail.com>
2022-03-17 15:40:23 +0300

    naygad

--- blockchain/contracts/RentalAgreement.sol
+++ blockchain/contracts/RentalAgreement.sol
@@ -174,8 +174,6 @@ contract RentalAgreement {
     }
 
     function endAgreement () public {
-        if (tenant_ == address(0)) revert("The contract is being in not allowed state");
-
         uint256 newBillingPeriod = (block.timestamp - rentStartTime_) / billingPeriodDuration_;
         if (currentBillingPeriod_ == newBillingPeriod || (newBillingPeriod - currentBillingPeriod_ == 1 && currentProfit_ >= rentalRate_)) {
             if (block.timestamp < rentEndTime_) revert("The contract is being in not allowed state");
