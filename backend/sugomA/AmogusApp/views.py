4af5cbd76f950fdc8bc14777439a9ab1682bf38a
0dminnimda <0dminnimda@gmail.com>
2022-03-18 00:45:48 +0300

    Update sugomA\AmogusApp\views.py: update /check

--- backend/sugomA/AmogusApp/views.py
+++ backend/sugomA/AmogusApp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from ..schema import code_smell
 
 
 # def MetaMask_auth(request):
@@ -11,13 +12,22 @@ def check(request):
     response = render(request, "home.html")
 
     # cookies
-    # a = int(request.COOKIES.get("a", "0"))
-    # print(a)
-    # response.set_cookie("a", str(a + 1))  # set_signed_cookie
+    a = int(request.COOKIES.get("a", "0"))
+    print(a)
+    response.set_cookie("a", str(a + 1))  # set_signed_cookie
+
+    # code_smell.storage = request.COOKIES
+    # print(request.COOKIES)
+    # print(code_smell)
+    # code_smell["a"] += 1
+
+    # for n, v in code_smell.storage.items():
+    #     response.set_cookie(n, v)
 
     # session
-    a = request.session.get("a", 0)
-    print(a)
-    request.session["a"] = a + 1
+    # print(request.session.session_key)
+    # a = request.session.get("a", 0)
+    # print(a)
+    # request.session["a"] = a + 1
 
     return response
