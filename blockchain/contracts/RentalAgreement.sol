e172ca8ceb450fc6e6577772010e40e8898c65f7
Gusarich <danielsedovzzz@gmail.com>
2022-03-17 16:26:26 +0300

    LETS GO

--- blockchain/contracts/RentalAgreement.sol
+++ blockchain/contracts/RentalAgreement.sol
@@ -182,8 +182,11 @@ contract RentalAgreement {
         }
         else {
             uint256 newBillingPeriod = (block.timestamp - rentStartTime_) / billingPeriodDuration_;
-            if (currentBillingPeriod_ == newBillingPeriod || currentProfit_ >= rentalRate_) revert("The contract is being in not allowed state");
-            else selfdestruct(landLord_);
+            if (currentBillingPeriod_ == newBillingPeriod || (newBillingPeriod - currentBillingPeriod_ == 1 && currentProfit_ >= rentalRate_)) revert("The contract is being in not allowed state");
+            else {
+                withdrawTenantProfit();
+                selfdestruct(landLord_);
+            }
         }
     }
 
